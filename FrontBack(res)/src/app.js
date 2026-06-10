5e3ec645c54e31a75e770565b0dae4f6ad4339c8
pro-pratham-18 <itz.prathathamesh18@gmail.com>
2026-06-10 21:43:34 +0530

    connection build

---
+++ FrontBack(res)/src/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    console.log("Get Request Hit");
+    res.json([
+        {
+            id:1,
+            name:"prathamesh",
+            age:20,
+
+        },
+        {
+            id:2,
+            name:"mohan",
+            age:25,
+
+        },
+        {
+            id:3,
+            name:"sohan",
+            age:19,
+
+        }
+    ]);
+});
+
+app.post('/post', (req, res) => {
+    const user = req.body;
+
+    res.json({
+        message: 'posted successfully ',
+        data: user
+    });
+});
+
+module.exports = app;
+
